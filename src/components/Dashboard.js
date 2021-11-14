168bd7af7245ec5b2c8a3feb5039f2da1f92eb0f
Kirk Woodill <zkcaptkirk@gmail.com>
2021-11-14 14:30:14 +1300

    Styling changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,12 +7,11 @@ function Dashboard() {
         {cards.map((card, x) => (
           <div className={`card-wrap ${ card.id }`} key={x}>
             <div className="img-container">
-              <img src="/images/icon-play.svg" alt="Play Icon"/>
             </div>
             <div className="info-container">
               <div className="wrapper">
                 <h2 className="card-title">{card.title}</h2>
-                <img src="/images/icon-ellipsis.svg" alt="Ellipsis icon"/>
+                <img src="./assets/images/icon-ellipsis.svg" alt="Ellipsis icon"/>
               </div>
               <div className="wrapper">
                 <span className="current-hrs">{card.timeframes.daily.current}hrs</span><br/>
