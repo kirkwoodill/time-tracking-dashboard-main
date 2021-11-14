a707b9b210b0104aa200c532e3f13a7c3c3072aa
Kirk Woodill <zkcaptkirk@gmail.com>
2021-11-14 16:44:52 +1300

    Tidy up of CRA files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from './logo.svg'
 import './App.scss'
 import Dashboard from './components/Dashboard'
-
 
 function App() {
   return (
     <>
-      
       <Dashboard />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Eit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </>
   );
 }
